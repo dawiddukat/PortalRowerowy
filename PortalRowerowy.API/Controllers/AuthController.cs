19d8fb73fadf872f5914f865232186303090879c
dawiddeedee <dawiddukat@gmail.com>
2020-01-22 23:28:35 +0100

    2/20 utworzenie DTO oraz uruchomienie debbugera

--- PortalRowerowy.API/Controllers/AuthController.cs
+++ PortalRowerowy.API/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PortalRowerowy.API.Data;
+using PortalRowerowy.API.Dtos;
 using PortalRowerowy.API.Models;
 
 namespace PortalRowerowy.API.Controllers
@@ -17,19 +18,19 @@ namespace PortalRowerowy.API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string username, string password)
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
-            username = username.ToLower(); //z małych liter użytkownik
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower(); //z małych liter użytkownik
 
-            if (await _repository.UserExist(username))
+            if (await _repository.UserExist(userForRegisterDto.Username))
                 return BadRequest("Użytkownik o takiej nazwie już istnieje!");
 
             var userToCreate = new User
             {
-                Username = username
+                Username = userForRegisterDto.Username
             };
 
-            var createdUser = await _repository.Register(userToCreate, password);
+            var createdUser = await _repository.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
         }
