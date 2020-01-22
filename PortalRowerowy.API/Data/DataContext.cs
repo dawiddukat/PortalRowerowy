d9dd6619394220ae32e7853c5acdb4832e9373c1
dawiddeedee <dawiddukat@gmail.com>
2020-01-22 16:53:13 +0100

    pierwszy

---
+++ PortalRowerowy.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PortalRowerowy.API.Models;
+
+namespace PortalRowerowy.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
