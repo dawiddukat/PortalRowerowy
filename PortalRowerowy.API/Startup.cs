2a28e2364ae029a0e61eb835cfd950b1d4da5798
dawiddeedee <dawiddukat@gmail.com>
2020-01-23 22:09:23 +0100

    Załadowanie danych testowych do DB

--- PortalRowerowy.API/Startup.cs
+++ PortalRowerowy.API/Startup.cs
@@ -33,6 +33,7 @@ namespace PortalRowerowy.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
+            services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                         .AddJwtBearer(options => {
@@ -47,13 +48,14 @@ namespace PortalRowerowy.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             //testowy2222
+            seeder.SeedUsers();
             app.UseCors( x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             app.UseAuthentication();
             app.UseMvc();
