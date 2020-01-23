89e45fe9fe66699b823272705aa26412d24c99fe
dawiddeedee <dawiddukat@gmail.com>
2020-01-23 11:36:13 +0100

    2 27 użycie usługi w komponencie

--- PortalRowerowy.SPA/src/app/_services/auth.service.ts
+++ PortalRowerowy.SPA/src/app/_services/auth.service.ts
@@ -16,7 +16,7 @@ login(model: any){
   return this.http.post(this.baseUrl + 'login', model)
     .pipe(map((response: any) =>{
       const user = response;
-      if(user){
+      if (user) {
         localStorage.setItem('token', user.token);
       }
 
