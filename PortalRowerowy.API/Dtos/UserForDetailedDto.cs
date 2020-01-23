6fbd881375b94d8f9f1486066f4dfddb32fe10ba
dawiddeedee <dawiddukat@gmail.com>
2020-01-24 00:53:23 +0100

    42 dodanie DTO

---
+++ PortalRowerowy.API/Dtos/UserForDetailedDto.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using PortalRowerowy.API.Models;
+
+namespace PortalRowerowy.API.Dtos
+{
+    public class UserForDetailedDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        //PODSTAWOWE INFORMACJE O UŻYTKOWNIKU
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string TypeBicycle { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Country { get; set; }
+        //public string Voivodeship { get; set; }
+        //public string County { get; set; }   
+        public string City { get; set; }
+        //DODATKOWE INFORMACJE - ZAKŁADKA INFO
+        public string Bicycles { get; set; }
+        public string Profession { get; set; }
+        //ZAKŁADKA O MNIE
+        public string Description { get; set; }
+        //Zakładka PASJE, ZAINTERESOWANIE
+        public string Interests { get; set; }
+        public string DreamBicycle { get; set; }
+        //ZAKŁADKA ZDJĘCIA
+        public ICollection<UserPhoto> UserPhotos { get; set; }
+        //Zakładka Ogłoszenia
+        public ICollection<SellBicycle> SellBicycles { get; set; }
+        //ZAKŁADKA WYPRAWY
+        public ICollection<Adventure> Adventures { get; set; }
+
+        public string PhotoUrl { get; set;}
+    }
+}
---
+++ PortalRowerowy.API/Dtos/UserForListDto.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace PortalRowerowy.API.Dtos
+{
+    public class UserForListDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        //PODSTAWOWE INFORMACJE O UŻYTKOWNIKU
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string TypeBicycle { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Country { get; set; }
+        //public string Voivodeship { get; set; }
+        //public string County { get; set; }   
+        public string City { get; set; }
+
+        public string PhotoUrl { get; set;}
+        
+    }
+}
