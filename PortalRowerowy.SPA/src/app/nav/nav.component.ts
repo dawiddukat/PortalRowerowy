fd206371f0649e1b232cfe0fa33aff3f5ed13e6e
dawiddeedee <dawiddukat@gmail.com>
2020-01-23 15:50:53 +0100

    2 35 Rozkod. tokena spersonalizowanie powitania

--- PortalRowerowy.SPA/src/app/nav/nav.component.ts
+++ PortalRowerowy.SPA/src/app/nav/nav.component.ts
@@ -14,7 +14,7 @@ export class NavComponent implements OnInit {
   model: any = {};
 
   // tslint:disable-next-line: no-shadowed-variable
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
