b67ea8f09a4eb8bcf5efceb00d9288c8e6120e32
dawiddeedee <dawiddukat@gmail.com>
2020-01-24 00:42:33 +0100

    2 41 dodanie UserController i zaktualizowanie DB

--- PortalRowerowy.API/Startup.cs
+++ PortalRowerowy.API/Startup.cs
@@ -31,7 +31,10 @@ namespace PortalRowerowy.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(opt => {
+                    opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
             services.AddCors();
             services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
