55c345ef1e32f35d501977298b2868c5347f4f15
dawiddeedee <dawiddukat@gmail.com>
2020-02-01 12:41:20 +0100

    72 EDYCJA UŻYTKOWNIKA W FORMULARZU FRONT

--- PortalRowerowy.SPA/src/app/users/user-edit/user-edit.component.ts
+++ PortalRowerowy.SPA/src/app/users/user-edit/user-edit.component.ts
@@ -3,6 +3,9 @@ import { User } from 'src/app/_models/user';
 import { ActivatedRoute } from '@angular/router';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
+import { error } from 'protractor';
 
 @Component({
   selector: 'app-user-edit',
@@ -20,7 +23,8 @@ export class UserEditComponent implements OnInit {
     }
   }
 
-  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService, private userService: UserService,
+              private authService: AuthService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -29,9 +33,14 @@ export class UserEditComponent implements OnInit {
   }
 
   updateUser() {
-    console.log(this.user);
-    this.alertify.success('Profil pomyślnie zaktualizowany.');
-    this.editForm.reset(this.user);
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user)
+      .subscribe(next => {
+        this.alertify.success('Profil pomyślnie zaktualizowano');
+        this.editForm.reset(this.user); },
+        error => {
+        this.alertify.error(error);
+      }
+      );
   }
 
 }
