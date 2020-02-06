adf42957f757569852ff1144583028eab145ad9a
dawiddeedee <dawiddukat@gmail.com>
2020-02-06 12:53:31 +0100

    87 reactive forms

--- PortalRowerowy.SPA/src/app/app.module.ts
+++ PortalRowerowy.SPA/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angu
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { JwtModule } from '@auth0/angular-jwt';
 import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { NgxGalleryModule } from 'ngx-gallery';
@@ -69,6 +69,7 @@ export class CustomHammerConfig extends HammerGestureConfig {
       HttpClientModule,
       BrowserAnimationsModule,
       FormsModule,
+      ReactiveFormsModule,
       NgxGalleryModule,
       JwtModule.forRoot({
          config: {
@@ -82,6 +83,7 @@ export class CustomHammerConfig extends HammerGestureConfig {
       BsDropdownModule.forRoot(),
       TabsModule.forRoot(),
       FileUploadModule,
+
    ],
    providers: [
       AuthService,
--- PortalRowerowy.SPA/src/app/register/register.component.ts
+++ PortalRowerowy.SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,18 +13,26 @@ export class RegisterComponent implements OnInit {
   //@Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl(),
+      
+    });
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
-      this.alertify.success("Rejestracja udana");
-    }, error => {
-      this.alertify.error("Wystąpił błąd rejestracji!");
-    });
+    // this.authService.register(this.model).subscribe(() => {
+    //   this.alertify.success("Rejestracja udana");
+    // }, error => {
+    //   this.alertify.error("Wystąpił błąd rejestracji!");
+    // });
+    console.log(this.registerForm.value);
 
   }
 
